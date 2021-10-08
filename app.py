0f30d9d5bd2a11bbaa99ee47f400fdda80d342ae
art <artich222@gmail.com>
2021-10-08 17:20:32 +0300

    hello

--- app.py
+++ app.py
@@ -1,13 +1,33 @@
 from flask import Flask, render_template, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 
+
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URL'] = 'sqlite://result.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///result.db'
 db = SQLAlchemy(app)
 
-@app.route('/')
-def index():
-    return render_template('index.html')
+
+class News(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    zag = db.Column(db.String, nullable=True)
+    tex = db.Column(db.Text, nullable=False)
+
+
+@app.route('/write', methods=['GET', 'POST'])
+def write():
+    if request.method == "POST":
+        a = request.form.get('Tem')
+        b = request.form.get('Mes')
+        c = News(zag=a, tex=b)
+        db.session.add(c)
+        db.session.commit()
+        return redirect('/view')
+    return render_template('write.html')
+
+@app.route('/view')
+def view():
+    a = News.query.order_by(News.id).all()
+    return render_template('view.html', a=a)
 
 
 app.run(host='127.0.0.1', debug=True)
